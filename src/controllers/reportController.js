55b93da1e504a4dab0a23f17812b68b16a49596c
kavindisathsarani <dksmanimendra0@gmail.com>
2025-10-01 19:49:16 +0530

    removed unwanted commented code segments

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,16 +1,3 @@
-// const Task = require('../models/Task');
-
-// exports.getReport = async (req, res, next) => {
-//   try {
-//     const totalTasks = await Task.countDocuments();
-//     const completed = await Task.countDocuments({ status: 'done' });
-//     const inProgress = await Task.countDocuments({ status: 'in-progress' });
-//     const todo = await Task.countDocuments({ status: 'todo' });
-
-//     res.json({ totalTasks, completed, inProgress, todo });
-//   } catch (err) { next(err); }
-// };
-
 const Task = require('../models/Task');
 const Team = require('../models/Team');
 
--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -90,21 +90,6 @@ exports.updateTask = async (req, res, next) => {
   }
 };
 
-// exports.updateTask = async (req, res, next) => {
-//   try {
-//     const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//     if (!task) return res.status(404).json({ message: 'Task not found' });
-
-//     // Notify assigned user if status changed
-//     if (req.body.status && task.assignedTo && req.app.locals.sockets.has(task.assignedTo.toString())) {
-//       const socketId = req.app.locals.sockets.get(task.assignedTo.toString());
-//       req.app.locals.io.to(socketId).emit('taskStatusUpdated', task);
-//       console.log('Socket event emitted: taskStatusUpdated for user', task.assignedTo);
-//     }
-
-//     res.json(task);
-//   } catch (err) { next(err); }
-// };
 
 exports.addComment = async (req, res, next) => {
   try {
@@ -141,51 +126,4 @@ exports.deleteTask = async (req, res, next) => {
 };
 
 
-// const Task = require('../models/Task');
-// const User = require('../models/User');
 
-// exports.createTask = async (req, res, next) => {
-//   try {
-//     const task = await Task.create(req.body);
-
-//     // Notify assigned user via Socket.IO
-//     if (req.body.assignedTo && req.app.locals.sockets.has(req.body.assignedTo)) {
-//       const socketId = req.app.locals.sockets.get(req.body.assignedTo);
-//       console.log("Socket event emitted: taskAssigned for user", req.body.assignedTo);
-//       req.app.locals.io.to(socketId).emit('taskAssigned', task);
-//     }
-
-//     res.status(201).json(task);
-//   } catch (err) { next(err); }
-// };
-
-// exports.getTasks = async (req, res, next) => {
-//   try {
-//     const tasks = await Task.find().populate('assignedTo', 'name email').populate('teamId', 'name');
-//     res.json(tasks);
-//   } catch (err) { next(err); }
-// };
-
-// exports.getTaskById = async (req, res, next) => {
-//   try {
-//     const task = await Task.findById(req.params.id).populate('assignedTo', 'name email').populate('teamId', 'name');
-//     if (!task) return res.status(404).json({ message: 'Task not found' });
-//     res.json(task);
-//   } catch (err) { next(err); }
-// };
-
-// exports.updateTask = async (req, res, next) => {
-//   try {
-//     const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//     if (!task) return res.status(404).json({ message: 'Task not found' });
-//     res.json(task);
-//   } catch (err) { next(err); }
-// };
-
-// exports.deleteTask = async (req, res, next) => {
-//   try {
-//     const task = await Task.findByIdAndDelete(req.params.id);
-//     if (!task) return res.status(404).json({ message: 'Task not found' });
-//     res.json({ message: 'Task deleted' });
-//   } catch (err) { next(err); }
-// };
